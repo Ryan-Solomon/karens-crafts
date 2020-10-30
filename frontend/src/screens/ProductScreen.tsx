507d2f2addc4f25ee4646ffcbeec23a826659066
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-10-30 18:06:46 -0500

    figured out how to pass rooute params and type them

--- frontend/src/screens/ProductScreen.tsx
+++ frontend/src/screens/ProductScreen.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import Rating from './../components/Rating';
+import products from './../products';
+import { RouteComponentProps } from 'react-router';
 
-const ProductScreen = () => {
+type TParams = { id: string };
+
+function ProductScreen({ match }: RouteComponentProps<TParams>) {
+  const product = products.find((p) => p._id === match.params.id);
   return <div>Product</div>;
-};
+}
 
 export default ProductScreen;
