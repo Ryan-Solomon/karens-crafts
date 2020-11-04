e00ec35919a3bfc7b45c361cbfbb448b3a4fe029
Ryan Solomon <ryanjsolomon24@gmail.com>
2020-11-03 19:50:48 -0600

    adds cart screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import CartScreen from './screens/CartScreen';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path='/' component={HomeScreen} />
           {/* @ts-ignore */}
           <Route path='/products/:id' component={ProductScreen} />
+          <Route path='/cart/:id?' component={CartScreen} />
         </Container>
       </main>
       <Footer />
